dc358a18feac62b75bdb7d00181cd10d86aedf8b
wanchao <471937306@qq.com>
2018-09-25 15:37:46 +0800

    golang 跨域

--- router/route.go
+++ router/route.go
@@ -9,17 +9,20 @@ import (
 	"github.com/gin-contrib/cors"
 	"time"
 )
+
 var allowOrigins = map[string]bool{
-	"http://admin.littlebug.vip":      true,
-	"https://admin.littlebug.vip":      true,
+	"http://admin.littlebug.vip":  true,
+	"https://admin.littlebug.vip": true,
+	//"http://localhost:8080":       true,
 }
+
 func Route(router *gin.Engine) {
 	//设置模板
 	//core.SetTemplate(router)
 
 	// 跨域
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:[]string{"*"},
+		AllowOrigins: []string{"*"},
 		AllowOriginFunc: func(origin string) bool {
 			return allowOrigins[origin]
 		},
@@ -31,7 +34,7 @@ func Route(router *gin.Engine) {
 			"Access-Token",
 			"Access-Control-Allow-Origin",
 		},
-		AllowCredentials: false,
+		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
 	}))
 
@@ -39,24 +42,24 @@ func Route(router *gin.Engine) {
 	router.Use(middleware.Ginrus())
 
 	//登录
-	router.POST("/login",core.Handle(admin.LoginPost))
+	router.POST("/login", core.Handle(admin.LoginPost))
 
 	//后台
 	blogAdmin := router.Group("/adm")
 	blogAdmin.Use(core.Middware(middleware.Token))
 	{
 		// 用户信息
-		blogAdmin.GET("/getUserInfo",core.Handle(admin.GetUserInfo))
+		blogAdmin.GET("/getUserInfo", core.Handle(admin.GetUserInfo))
 
 		// 文章管理
-		blogAdmin.POST("/articleList",core.Handle(admin.ArticleList))
-		blogAdmin.POST("/storeArticle",core.Handle(admin.CreateArticle))
-		blogAdmin.GET("/deleteArticle",core.Handle(admin.DeleteArticle))
+		blogAdmin.POST("/articleList", core.Handle(admin.ArticleList))
+		blogAdmin.POST("/storeArticle", core.Handle(admin.CreateArticle))
+		blogAdmin.GET("/deleteArticle", core.Handle(admin.DeleteArticle))
 
 		// 文章分类
-		blogAdmin.GET("/categories",core.Handle(admin.Categories))
+		blogAdmin.GET("/categories", core.Handle(admin.Categories))
 		//blogAdmin.GET("/deleteCategory",core.Handle(admin.DeleteCategory)) // 删
-		blogAdmin.GET("/createCategory",core.Handle(admin.CreateCategory)) // 增
+		blogAdmin.GET("/createCategory", core.Handle(admin.CreateCategory)) // 增
 
 	}
 
